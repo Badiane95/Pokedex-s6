5a244568082130b75d3a9c496d8cdba2001ea0b3
Badiane95 <165165737+Badiane95@users.noreply.github.com>
2026-07-05 20:42:29 +0200

    Update tyradex.js

--- github-actions/partie-3/src/api/tyradex.js
+++ github-actions/partie-3/src/api/tyradex.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 
-const api = axios.create({
-    baseURL: "/api",
-});
+const API_BASE = "https://tyradex.vercel.app";
 
 export const fetchPokemonForGeneration = async (generation = 1) => {
     let listPokemon = [];
     try {
-        const req = await api.get(`/v1/gen/${generation}`);
+        const req = await axios.get(`${API_BASE}/api/v1/gen/${generation}`);
         listPokemon = req.data;
 
         const serverErrorStartNumber = 400;
@@ -29,7 +27,7 @@ export const fetchPokemonForGeneration = async (generation = 1) => {
 export const fetchPokemon = async (pkmnId, region = null) => {
     try {
         const regionName = region ? `/${region}` : "";
-        const req = await api.get(`/v1/pokemon/${pkmnId}${regionName}`);
+        const req = await axios.get(`${API_BASE}/api/v1/pokemon/${pkmnId}${regionName}`);
 
         return req.data;
     } catch (error) {
@@ -39,7 +37,7 @@ export const fetchPokemon = async (pkmnId, region = null) => {
 
 export const fetchAllTypes = async () => {
     try {
-        const req = await api.get("/v1/types");
+        const req = await axios.get(`${API_BASE}/api/v1/types`);
 
         return req.data;
     } catch (error) {
